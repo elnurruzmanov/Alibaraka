375b40b9e8c5df197d44f629ba68addf70f61e30
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-08-06 04:53:18 +0300

    now i will add animations

--- alibaraka/src/components/Register/Register.jsx
+++ alibaraka/src/components/Register/Register.jsx
@@ -11,6 +11,7 @@ const Register = () => {
     handleSubmit,
     formState: { errors },
     reset,
+    trigger,
   } = useForm();
 
   const onSubmit = (data) => {
@@ -18,7 +19,10 @@ const Register = () => {
     reset();
   };
 
-  console.log(errors);
+  const onError = (errors) => {
+    console.log(errors);
+  };
+  // console.log(errors);
 
   return (
     <>
@@ -30,18 +34,19 @@ const Register = () => {
             Agar sizda biror taklif yoki savollar bo'lsa, mana shu yerda yozib
             qoldiring, biz doim javob berishga shaymiz.
           </p>
-          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <form className="form" onSubmit={handleSubmit(onSubmit, onError)}>
             <div className="form-input">
               <label className="contact-label">
                 <input
                   type="text"
                   placeholder="Ismingizni kiriting"
                   required
-                  className="inputs"
-                  {...register("name", { required: "Name is required", min:{
-                    value:12,
-                    message:"Ism kiritilmagan hali"
-                  } })}
+                  className={`inputs ${errors.name && "invalid"}`}
+                  {...register("name", { required: " " })}
+                  onKeyUp={() => {
+                    trigger("name");
+                  }}
+                  //
                 />
                 {errors.name && (
                   <small className="text-danger">{errors.name.message}</small>
@@ -52,8 +57,17 @@ const Register = () => {
                   type="number"
                   placeholder="+375259577762"
                   required
-                  className="inputs"
-                  {...register("number", { required: "Number is required" })}
+                  className={`inputs ${errors.number && "invalid"}`}
+                  {...register("number", {
+                    required: "Number is required",
+                    pattern: {
+                      value: /^[0-9]*$/,
+                      message: "Invalid phone no",
+                    },
+                  })}
+                  onKeyUp={() => {
+                    trigger("number");
+                  }}
                 />
               </label>
 
@@ -61,8 +75,21 @@ const Register = () => {
                 placeholder="Murojaatni kiriting"
                 id="textarea"
                 name="textarea"
-                className="contact-textarea"
-                {...register("textarea", { required: "Number is required" })}
+                className={`contact-textarea ${errors.textarea && "invalid"}`}
+                {...register("textarea", {
+                  required: "Number is required",
+                  minLength: {
+                    value: 10,
+                    message: "Minimum Required length is 10",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "Maximum allowed length is 50",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("textarea");
+                }}
               ></textarea>
             </div>
             <button type="submit" className="form-btn">
